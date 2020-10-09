ab5b032c4595c8d1612e89e0abdc8361cf3d18f0
lorena hernandez <lpaulette.hernandez@edu.uag.mx>
2020-10-09 16:12:28 -0500

    3erfuncion

--- PRACTICAS/LAB01/GENFUN.c
+++ PRACTICAS/LAB01/GENFUN.c
@@ -6,13 +6,16 @@
 void main ( void )
 {
 	uint8 var[6]="renata";
+	uint8 var2[0]=" ";
+
 	GENFUN_vCapsOn(&var,6);
+	
 	printf(" cambio %s\n",var);
-
-
-
+	var2[0]=GENFUN_u8GetOccurence(var,'A',6);
+	printf(" cambio %c\n",var2[0]);
 
 }
+
 
 void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList) 
 {
@@ -30,5 +33,63 @@ void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList)
 		pu8Src ++;
 		u8SizeOfList --;
 	}
+}
+
+void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList) 
+{
+
+
+
+}
+
+uint8 GENFUN_u8GetOccurence (uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList) 
+{
+	uint8 var2[0];
+
+	int j=0;
+	while(u8SizeOfList!=0)
+	{
+		if(*pu8Src==u8Target)
+		{
+			
+			j++;
+
+		}
+		pu8Src ++;	
+		u8SizeOfList --;
+	}
+	var2[0]='2';
+	return var2[0];
+}
+
+uint8 GENFUN_u8GetAverage (uint8 *pu8Src, uint8 u8SizeOfList) 
+{
+
+
+}
+
+void GENFUN_u8MemSet (uint8 *pu8Src, uint8 u8Char2Set, uint8 u8SizeOfList) 
+{
+
+
+
+
+}
+
+void GENFUN_vSortList (uint8 *pu8Src, uint8 *pu8Dest, uint8 u8SizeOfList) 
+{
+
+
+}
+
+void GENFUN_vSoftSignal (uint8 *pu8Src, uint8 *pu8Dest) 
+{
+
+
+}
+
+void GENFUN_vFilterSignal (uint8 *pu8Src, uint8 *pu8Dest, uint8 u8MaxVal, uint8 u8MinVal) 
+{
+
 
 }
--- PRACTICAS/LAB01/GENFUN.h
+++ PRACTICAS/LAB01/GENFUN.h
@@ -1,9 +1,13 @@
 typedef unsigned char uint8;
 typedef unsigned short uint16;
+
 #define INICIOASCII_MINUSC 96
 #define INICIOASCII_MAYUSC 123
 #define FACTORCONVERSASCII 32
 
 
 void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList);
-
+void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList);
+uint8 GENFUN_u8GetOccurence (uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList);
+uint8 GENFUN_u8GetAverage (uint8 *pu8Src, uint8 u8SizeOfList);
+ 
