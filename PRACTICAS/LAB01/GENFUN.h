ed91b18871558133a62bd27aa546a04225c32363
lorena hernandez <lpaulette.hernandez@edu.uag.mx>
2020-10-10 11:04:38 -0500

    changes

--- PRACTICAS/LAB01/GENFUN.h
+++ PRACTICAS/LAB01/GENFUN.h
@@ -1,12 +1,13 @@
 typedef unsigned char uint8;
 typedef unsigned short uint16;
 
-#define INICIOASCII_MINUSC 96
-#define INICIOASCII_MAYUSC 123
 #define FACTORCONVERSASCII 32
 #define LOWER_BOUND         80
 #define ASCII_CODE_MSET    36
-#define ASCII_LOW_TRESHOLD_TO_ON     65    
+#define ASCII_LOW_TRESHOLD_TO_ON     65 
+#define ASCII_HIGH_THRESHOLD_CAPSON_TO_OFF        123            
+#define ASCII_LOW_THRESHOLD_CAPSON_TO_OFF        96            
+   
 
 
 void GENFUN_vCapsOn ( uint8 *pu8Src, uint8 u8SizeOfList ); 
@@ -19,6 +20,4 @@ void GENFUN_vSortList (uint8 *pu8Src, uint8 *pu8Dest, uint8 u8SizeOfList);
 void GENFUN_vSoftSignal (uint8 *pu8Src, uint8 *pu8Dest); 
 void GENFUN_vFilterSignal (uint8 *pu8Src, uint8 *pu8Dest, uint8 u8MaxVal, uint8 u8MinVal); 
 
-#define ASCII_HIGH_THRESHOLD_CAPSON_TO_OFF        122            
-#define ASCII_LOW_THRESHOLD_CAPSON_TO_OFF        97            
 
