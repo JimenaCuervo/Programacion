b7faef589183de79960e76adc25ccfc23809342f
lorena hernandez <lpaulette.hernandez@edu.uag.mx>
2020-10-10 10:27:27 -0500

    funciones

--- PRACTICAS/LAB01/GENFUN.c
+++ PRACTICAS/LAB01/GENFUN.c
@@ -34,13 +34,100 @@ void GENFUN_vCapsOn (uint8 *pu8Src, uint8 u8SizeOfList)
 		u8SizeOfList --;
 	}
 }
+void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList)
 
-void GENFUN_vCapsOff (uint8 *pu8Src, uint8 u8SizeOfList) 
-{
+ {
 
+     while( u8SizeOfList != 0 )
 
+     {
 
-}
+         if( *pu8Src >= ASCII_LOW_TRESHOLD_TO_ON && *pu8Src <= ASCII_HIGH_TRESHOLD_TO_ON )
+
+         {
+
+             *pu8Src += ASCII_CONVERSION_FACTOR    ;
+
+         }
+
+         pu8Src++;
+
+         u8SizeOfList--;
+
+     }
+
+ }
+ 
+uint8 GENFUN_u8GetOccurrence ( uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList )
+
+ {
+
+     uint8 u8ArrayOcurrences = 0;
+ 
+    while ( u8SizeOfList != 0 )
+
+     {
+
+         if( *pu8Src ==  u8Target )
+
+         {
+
+             u8ArrayOcurrences++;
+
+         }
+
+     pu8Src++;
+
+     u8SizeOfList--;
+
+     }
+
+ return u8ArrayOcurrences;
+
+ }
+ 
+uint8 GENFUN_u8GetAverage( uint8 *pu8Src, uint8 u8SizeOfList )
+
+ {
+
+     uint8 AverageOfArray = 0;
+ 
+    while ( u8SizeOfList != 0 )
+
+     {
+
+         AverageOfArray += *pu8Src;
+
+         pu8Src++;
+
+         u8SizeOfList--;
+
+     }
+
+     AverageOfArray = AverageOfArray / u8SizeOfList;
+
+     return AverageOfArray;
+
+ }
+ 
+void GENFUN_u8MemSet (uint8 *pu8Src, uint8 u8Char2Set, uint8 u8SizeOfList)
+
+ {
+
+     uint8 *pu8Dest = pu8Src;
+ 
+    while ( u8SizeOfList-- > 0 )
+
+     {
+
+         *pu8Dest++ = u8Char2Set;
+
+     }
+
+     return *pu8Src;
+
+ }
+
 
 uint8 GENFUN_u8GetOccurence (uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList) 
 {
@@ -62,34 +149,3 @@ uint8 GENFUN_u8GetOccurence (uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList)
 	return var2[0];
 }
 
-uint8 GENFUN_u8GetAverage (uint8 *pu8Src, uint8 u8SizeOfList) 
-{
-
-
-}
-
-void GENFUN_u8MemSet (uint8 *pu8Src, uint8 u8Char2Set, uint8 u8SizeOfList) 
-{
-
-
-
-
-}
-
-void GENFUN_vSortList (uint8 *pu8Src, uint8 *pu8Dest, uint8 u8SizeOfList) 
-{
-
-
-}
-
-void GENFUN_vSoftSignal (uint8 *pu8Src, uint8 *pu8Dest) 
-{
-
-
-}
-
-void GENFUN_vFilterSignal (uint8 *pu8Src, uint8 *pu8Dest, uint8 u8MaxVal, uint8 u8MinVal) 
-{
-
-
-}
